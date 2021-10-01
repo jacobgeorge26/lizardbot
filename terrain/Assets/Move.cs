9081706985f134501bccee41f59380daff64313c
Jacob Brown <jacobgeorgebrown26@gmail.com>
2021-10-01 16:54:12 +0100

    WIP rotation maths

--- terrain/Assets/Move.cs
+++ terrain/Assets/Move.cs
@@ -28,7 +28,6 @@ public class Move : MonoBehaviour
     //TO DO: make angle a parameter
     private void CoilBody()
     {
-        bool increase = true;
         double angle = Math.PI / 10;
 
         for (int index = 0; index < Joints.Count; index++)
@@ -36,39 +35,43 @@ public class Move : MonoBehaviour
             GameObject joint = Joints.ElementAt(index); //get current joint and section
             GameObject section = Sections.ElementAt(index);
 
-            //if they aren't the ones immediately after the head then they need to update their position
-            //this is to reflect from the previous iterations rotation
-            if (index > 0)
-            {
-                joint.transform.localPosition = RotateAroundYAxis(joint.transform.position, angle);
-                section.transform.localPosition = RotateAroundYAxis(section.transform.position, angle);
-            }
+            //rotate the joint
+            joint.transform.Rotate(0, (float)angle, 0, Space.Self);
 
-            //rotate to coil
-            if (index % 2 == 0) //for wide coil
-            {
-                //determine if the coil is going clockwise or anticlockwise
-                angle = increase ? angle : -1 * angle;
-                increase = !increase;
-
-                //rotate the joint
-                joint.transform.Rotate(0, (float)angle, 0, Space.Self);
-                
-                //update the position of the coupled section to reflect this
-                section.transform.localPosition = RotateAroundYAxis(section.transform.position, angle);
-                section.transform.Rotate(0, (float)angle, 0, Space.Self);
-            }
+            //update the position of the coupled section to reflect this
+            section.transform.Rotate(0, (float)angle, 0, Space.Self);
+            section.transform.position = RotateAroundYAxis(joint, section, angle);
         }
-        //coiling = Math.Abs(Joints[0].transform.localRotation.y) < Math.PI / 6;
         coiling = false; //testing - only do one rotation
     }
 
-    //determine the new position of an object after being rotated x *radians* around the y axis
-    private Vector3 RotateAroundYAxis(Vector3 position, double angle)
+    private void adjustRemainingBody(int originalIndex, double angle)
     {
-        float x = (float)((position.x * Math.Cos(angle)) + (position.z * Math.Sin(angle)));
-        float y = position.y;
-        float z = (float)((position.z * Math.Cos(angle)) - (position.x * Math.Sin(angle)));
+        for (int index = originalIndex + 1; index < Joints.Count; index++)
+        {
+            GameObject joint = Joints.ElementAt(index); //get current joint and section
+            GameObject section = Sections.ElementAt(index);
+
+            //rotate the joint
+            joint.transform.Rotate(0, (float)angle, 0, Space.Self);
+
+            //update the position of the coupled section to reflect this
+            section.transform.Rotate(0, (float)angle, 0, Space.Self);
+            section.transform.position = RotateAroundYAxis(joint, section, angle);
+        }
+    }
+
+    //determine the new position of an object after being rotated x *radians* around the y axis
+    private Vector3 RotateAroundYAxis(GameObject origin, GameObject point, double angle)
+    {
+        Vector3 translation = new Vector3(point.transform.position.x - origin.transform.position.x + (origin.transform.localScale.x), 0f, point.transform.position.z - origin.transform.position.z + (origin.transform.localScale.z));
+        point.transform.position -= translation;
+        //the position needs to be adjusted to the CENTRE of the joint
+        //so pass that in
+        float x = (float)((point.transform.position.x * Math.Cos(angle)) + (point.transform.position.z * Math.Sin(angle)));
+        float y = point.transform.position.y;
+        float z = (float)((point.transform.position.z * Math.Cos(angle)) - (point.transform.position.x * Math.Sin(angle)));
+        point.transform.position += translation;
         return new Vector3(x, y, z);
     }
 }
