e6074beae1880dcc406d590b903c3694d69e5513
Jacob Brown <jacobgeorgebrown26@gmail.com>
2021-10-04 14:33:54 +0100

    Coupled body parts - WIP uncoiling
    WIP relative angle

--- terrain/Assets/Move.cs
+++ terrain/Assets/Move.cs
@@ -9,7 +9,13 @@ public class Move : MonoBehaviour
     public GameObject Head;
     public List<GameObject> Joints;
     public List<GameObject> Sections;
+    public GameObject Camera;
     bool coiling = true;
+    bool uncoiling = true;
+    private double coilAngle = 10;
+    private int turncount = 12;
+
+    private bool uncoiltest = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,60 +24,110 @@ public class Move : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (coiling) CoilBody();
+    { 
+        if (coiling) Coil();
+        //else if (uncoiling) Uncoil();
     }
 
     //iterate through each joint / section and rotate it
     //every other joint will not rotate to produce a wide coil
     //TO DO: make coil size dynamic
     //TO DO: make angle a parameter
-    private void CoilBody()
-    {
-        double angle = Math.PI / 10;
-
+    private void Coil()
+    {     
+        ToggleBodyLock(0, true, LockOption.Backward);
         for (int index = 0; index < Joints.Count; index++)
         {
-            GameObject joint = Joints.ElementAt(index); //get current joint and section
-            GameObject section = Sections.ElementAt(index);
-
-            //rotate the joint
-            joint.transform.Rotate(0, (float)angle, 0, Space.Self);
-
-            //update the position of the coupled section to reflect this
-            section.transform.Rotate(0, (float)angle, 0, Space.Self);
-            section.transform.position = RotateAroundYAxis(joint, section, angle);
+            if (index % 2 == 0)
+            {
+                GameObject joint = Joints[index];
+                double angle = index % 4 == 0 ? coilAngle * -0.5 : coilAngle;
+                joint.transform.Rotate(0, (float)angle, 0, Space.Self);
+                turncount--;
+                System.Threading.Thread.Sleep(25);                    
+            }  
         }
-        coiling = false; //testing - only do one rotation
+        coiling = turncount > 0;
+        ToggleBodyLock(0, false, LockOption.Backward);
     }
 
-    private void adjustRemainingBody(int originalIndex, double angle)
+    private void Uncoil()
     {
-        for (int index = originalIndex + 1; index < Joints.Count; index++)
+        Vector3[] relativeAngles = new Vector3[Joints.Count];
+        for (int joint = 0; joint < Joints.Count; joint++)
         {
-            GameObject joint = Joints.ElementAt(index); //get current joint and section
-            GameObject section = Sections.ElementAt(index);
-
-            //rotate the joint
-            joint.transform.Rotate(0, (float)angle, 0, Space.Self);
-
-            //update the position of the coupled section to reflect this
-            section.transform.Rotate(0, (float)angle, 0, Space.Self);
-            section.transform.position = RotateAroundYAxis(joint, section, angle);
+            relativeAngles[joint] = GetRelativeRotation(joint);
+            Debug.Log($"Joint: {joint + 1}; Angle: {relativeAngles[joint].y}");
         }
+        ToggleBodyLock(Joints.Count - 1, true, LockOption.Forward);       
+
+        for (int index = Joints.Count - 1; index >= 0; index--)
+        {
+            GameObject joint = Joints[index];
+
+            if (Math.Abs(relativeAngles[index].y) > coilAngle)
+            {
+                double angle = coilAngle;
+                //double angle = joint.transform.rotation.y > 0 ? coilAngle * -1 : coilAngle;
+                int count = 0;
+
+                while (Math.Abs(relativeAngles[index].y) > Quaternion.Euler(0, (float)coilAngle, 0).y && count < 100)
+                {
+                    joint.transform.Rotate(0, (float)angle, 0, Space.Self);
+                    relativeAngles[index].y += (float)angle;
+                    System.Threading.Thread.Sleep(25);
+                    count++;
+                }
+
+            }
+
+        }
+        uncoiling = false;
+        //uncoiling = Math.Abs(Joints[0].transform.rotation.y) > Quaternion.Euler(0, (float)coilAngle, 0).y; //do once while testing
+
+        ToggleBodyLock(Joints.Count - 1, false, LockOption.Forward);
     }
 
-    //determine the new position of an object after being rotated x *radians* around the y axis
-    private Vector3 RotateAroundYAxis(GameObject origin, GameObject point, double angle)
+    //MUST be unlocked to work
+    private Vector3 GetRelativeRotation(int index)
     {
-        Vector3 translation = new Vector3(point.transform.position.x - origin.transform.position.x + (origin.transform.localScale.x), 0f, point.transform.position.z - origin.transform.position.z + (origin.transform.localScale.z));
-        point.transform.position -= translation;
-        //the position needs to be adjusted to the CENTRE of the joint
-        //so pass that in
-        float x = (float)((point.transform.position.x * Math.Cos(angle)) + (point.transform.position.z * Math.Sin(angle)));
-        float y = point.transform.position.y;
-        float z = (float)((point.transform.position.z * Math.Cos(angle)) - (point.transform.position.x * Math.Sin(angle)));
-        point.transform.position += translation;
-        return new Vector3(x, y, z);
+        Vector3 relativeAngles = new Vector3(0, 0, 0);
+        GameObject joint = Joints[index];
+        GameObject section = index > 0 ? Sections[index - 1] : Head;      
+        relativeAngles.y = 360 - section.transform.rotation.eulerAngles.y;
+        relativeAngles.y = relativeAngles.y > 180 ? relativeAngles.y - 360 : relativeAngles.y;        
+        return relativeAngles;
     }
+
+    private void ToggleBodyLock(int bodyIndex, bool lockBody, LockOption direction)
+    {
+        if (direction == LockOption.Backward)
+        {
+            Camera.transform.parent = Head.transform;
+            for (int index = bodyIndex; index < Joints.Count; index++)
+            {
+                Joints[index].transform.parent = index > 0 && lockBody ? Sections[index - 1].transform : this.transform;
+                Sections[index].transform.parent = lockBody ? Joints[index].transform : this.transform;
+            }
+        }
+        else if(direction == LockOption.Forward)
+        {
+            Camera.transform.parent = Sections.Last().transform;
+            for (int index = bodyIndex; index >= 0; index--)
+            {
+                Joints[index].transform.parent = lockBody ? Sections[index].transform : this.transform;
+                Sections[index].transform.parent = index < Joints.Count - 1 && lockBody ?  Joints[index + 1].transform : this.transform;
+            }
+            Head.transform.parent = lockBody ? Joints[0].transform : this.transform;
+        }
+
+    }
+
+    enum LockOption
+    {
+        Forward = 0,
+        Backward = 1,
+        Unlocked = 2
+    }
+
 }
