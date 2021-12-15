b071df652725b2b3229f1bdee699c0f431bd7ff6
Jacob Brown <87342120+jacobgeorge26@users.noreply.github.com>
2021-12-15 16:36:06 +0000

    Improved GenerateRobot
    - split into methods
    - added validation
    - added default params
    - added slider on settings menu

--- terrain/Assets/Scripts/GenerateRobot.cs
+++ terrain/Assets/Scripts/GenerateRobot.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Config;
+using System;
 
 public class GenerateRobot : BaseConfig
 {
+    [SerializeField]
+    [InspectorName("Use Default Configuration")]
+    protected bool isDefault;
+
     void Start()
     {
         //setup overall robot
@@ -12,10 +17,69 @@ public class GenerateRobot : BaseConfig
         robot.name = "robot";
         robot.transform.position = new Vector3(0, 3, -8);
 
+        if (isDefault) DefaultParams();
+        else if (!ValidateParams()) return;
+
+        SetupBody(robot);
+
+        //TODO: setup legs
+
+        SetupCam(robot);
+    }
+
+    private void DefaultParams()
+    {
+        NoSections = 5;
+        DrivingSections = new bool[5] { true, true, true, true, true };
+        RotatingSections = new bool[5] { true, false, true, false, true };
+        TurnVelocity = 180;
+        DriveVelocity = 1f;
+        MaxAngle = new int[3] { 30, 60, 45 };
+    }
+
+    private bool ValidateParams()
+    {
+        if(NoSections < 1)
+        {
+            Debug.LogError("The minimum number of body sections is 1");
+            return false;
+        }
+        if (RotatingSections.Length != NoSections)
+        {
+            Debug.LogError("The number of rotating sections must equal the number of sections");
+            return false;
+        }
+        if(DrivingSections.Length != NoSections)
+        {
+            Debug.LogError("The number of driving sections must equal the number of sections");
+            return false;
+        }
+        //TODO: update once individual customisation is setup - will need to look at each one - drive & turn
+        if(TurnVelocity < 1)
+        {
+            Debug.LogWarning("The rotating sections will not rotate due to the turn velocity");
+        }
+        if(DriveVelocity < 0.1f)
+        {
+            Debug.LogWarning("The driving sections will not rotate due to the drive velocity");
+        }
+        //TODO: add more info about which section and angle it is
+        foreach(int angle in MaxAngle)
+        {
+            if(angle > 60)
+            {
+                Debug.LogWarning("A maximum angle is >60 which may produce unstable results");
+            }
+        }
+        return true;
+    }
+
+    private void SetupBody(GameObject robot)
+    {
         for (int i = 0; i < NoSections; i++)
         {
-            GameObject section = i == 0 
-                ? MonoBehaviour.Instantiate(Resources.Load<GameObject>("Head")) 
+            GameObject section = i == 0
+                ? MonoBehaviour.Instantiate(Resources.Load<GameObject>("Head"))
                 : MonoBehaviour.Instantiate(Resources.Load<GameObject>("Section"));
             section.name = i == 0 ? "head" : $"section{i}";
             Sections.Add(section);
@@ -26,7 +90,7 @@ public class GenerateRobot : BaseConfig
             BodyConfig config = section.GetComponent<BodyConfig>();
             config.IsRotating = RotatingSections[i];
             config.IsClockwise = new bool[3] { false, true, false }; //TODO: make this dynamic
-            config.MaxAngle = MaxAngle; //TODO: make this better dynamic
+            config.MaxAngle = MaxAngle;
             config.TurnVelocity = TurnVelocity;
             config.IsDriving = DrivingSections[i];
             config.DriveVelocity = DriveVelocity;
@@ -38,8 +102,10 @@ public class GenerateRobot : BaseConfig
                 joint.connectedBody = Sections[i - 1].GetComponent<Rigidbody>();
             }
         }
+    }
 
-        //setup camera
+    private void SetupCam(GameObject robot)
+    {
         GameObject cam = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Robot Camera"));
         cam.name = "camera";
         cam.transform.parent = robot.transform;
--- terrain/Assets/Settings.cs
+++ terrain/Assets/Settings.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Settings : MonoBehaviour
 {
+    //when the generate robot button has been clicked 
+    //pass the params - if not default then move to customisation
+    //if default then move to the terrain scene
     public void GenerateRobot()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        GenerateRobot controller = FindObjectOfType<GenerateRobot>();
+        
     }
+
+
 }
