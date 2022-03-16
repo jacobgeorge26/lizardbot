97da014bd27726c56151d7a9ac701c8dea6ace59
Jacob Brown <87342120+jacobgeorge26@users.noreply.github.com>
2022-03-16 23:27:36 +0000

    Added edges to plane

--- terrain/Assets/Scripts/GenerateTerrain.cs
+++ terrain/Assets/Scripts/GenerateTerrain.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class GenerateTerrain : MonoBehaviour
@@ -19,8 +20,18 @@ public class GenerateTerrain : MonoBehaviour
             GameObject terrain = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Terrain"));
             terrain.SetActive(false);
             terrain.name = $"Terrain {i + 1}";
+            //index = Int32.Parse(Regex.Match(gameObject.name, @"\d+").Value) - 1;
             terrain.transform.parent = gameObject.transform;
             terrain.transform.position = GetPosition(i, terrain);
+            foreach (Transform child in terrain.transform)
+            {
+                if (child.gameObject.name.Contains("Plane"))
+                {
+                    int planeNo = Int32.Parse(Regex.Match(child.gameObject.name, @"\d+").Value);
+                    int posMultiplier = planeNo % 2 == 0 ? 1 : -1;
+                    child.localPosition = new Vector3(planeNo < 3 ? posMultiplier * TerrainConfig.GetTerrainWidth() / 2f : 0, 0, planeNo > 2 ? posMultiplier * TerrainConfig.GetTerrainWidth() / 2f : 0);
+                }
+            }
             terrain.SetActive(true);
         }
         CameraConfig.OverviewCamera.transform.position = new Vector3((TerrainConfig.minX() + TerrainConfig.maxX()) / 2, (TerrainConfig.minY() + TerrainConfig.maxY()) / 2, (TerrainConfig.minZ() + TerrainConfig.maxZ()) / 2);
